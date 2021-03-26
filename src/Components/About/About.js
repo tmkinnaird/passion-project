b5c8adfb81223536a3c4016368bb0bd02c1398b3
Tyler Kinnaird <tmkinnaird87@gmail.com>
2021-03-26 11:08:45 -0600

    descriptions of pages added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,7 +4,11 @@ const About = () => {
     return(
         <div>
             <h1>About Green Thumb</h1>
-            <p>Green Thumb is a great place to learn when and where to get your favorite plants in the ground. We love seeing people cultivate their own gardens, whether it be in the yard or on a larger scale. We are here to help you learn the best way to make a perfect garden. Vegetables that we can eat, or just a beautiful landscape, we are here to help.</p>
+            <p>Green Thumb is a great place to learn when and where to get your favorite plants in the ground.</p>
+            <p>We love seeing people cultivate their own gardens, whether it be in the yard or on a larger scale. </p> 
+            <p>We are here to help you learn the best way to make a perfect garden. </p>
+            <p>Vegetables that we can eat, or just a beautiful landscape, we are here to help.</p>
+            
         </div>
     )
 }
